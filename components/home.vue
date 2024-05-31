9016df7bc66710be7b06d29b4c63e98ede5c7a3d
zhiqing <mail@201lab.top>
2024-05-31 22:38:25 +0800

    update webui

--- components/home.vue
+++ components/home.vue
@@ -1,33 +1,59 @@
 <template>
     <a-layout class="layout">
         <a-layout-header style="background: #FF3C37; display: flex; align-items: center; justify-content: space-between;">
-            <p style="color: white; font-size: 2em; margin: 0; " >ChatTTS WebUI</p>
+            <div style="display: flex; align-items: center;">
+                <img src="/public/favicon.ico" alt="icon" style="width: 3em; height: 3em; margin-right: 10px;" />
+                <p style="color: white; font-size: 2em; margin: 0;">ChatTTS WebUI</p>
+            </div>
             <a-button type="default" @click="toggleLanguage" style="color: black; border: none;">
                 {{ language === 'en' ? '切换到中文' : 'Switch to English' }}
             </a-button>
         </a-layout-header>
         <a-layout-content style="padding: 20px 50px;">
             <div style="display: flex; align-items: center; width: 100%; justify-content: center;">
-                <a-textarea v-model:value="text" :rows="4" :placeholder="language === 'en' ? 'Input text...' : '输入文字...'" style="width: 30%; min-width: 300px;border-radius: 7px;" />
+                <a-textarea v-model:value="text" :rows="5" :placeholder="language === 'en' ? 'Input text...' : '输入文字...'" style="width: 30%; min-width: 300px;border-radius: 7px; font-size: 27px" />
                 <div style="display: flex; flex-direction: column; margin-left: 10px;">
                     <div style="display: flex; margin-bottom: 10px;">
-                        <a-button type="primary" style="margin-right: 10px;" @click="addLaugh">{{ language === 'en' ? 'Add Laugh' : '加笑声' }}</a-button>
-                        <a-button type="primary" @click="addBreak">{{ language === 'en' ? 'Add Break' : '加停顿' }}</a-button>
+                        <a-button type="primary" style="margin-right: 10px;width: 100px;" @click="addLaugh">{{ language === 'en' ? 'Add Laugh' : '加笑声' }}</a-button>
+                        <a-button type="primary" style="width: 100px;" @click="addBreak">{{ language === 'en' ? 'Add Break' : '加停顿' }}</a-button>
                         <a-radio-group v-model:value="voice" style="display: flex; margin-left: 10px;">
-                            <a-radio-button value="man">{{ language === 'en' ? 'Male Voice' : '男声' }}</a-radio-button>
-                            <a-radio-button value="woman">{{ language === 'en' ? 'Female Voice' : '女声' }}</a-radio-button>
+                            <a-radio-button :value="voice_adj === 2222 ? 'man' : null" value="man" style="width: 120px; text-align: center;" @click="setVoiceAdj(2222)">{{ language === 'en' ? 'Male Voice' : '男声' }}</a-radio-button>
+                            <a-radio-button :value="voice_adj === 6615 ? 'woman' : null" value="woman" style="width: 120px; text-align: center;" @click="setVoiceAdj(6615)">{{ language === 'en' ? 'Female Voice' : '女声' }}</a-radio-button>
                         </a-radio-group>
                     </div>
-                    <a-button type="primary" style="width: 100%;" @click="generate">{{ language === 'en' ? 'Generate' : '生成' }}</a-button>
+                    <div style="display: flex; align-items: center; margin-bottom: 10px;">
+                        <span style="margin-right: 10px;">{{ language === 'en' ? 'Voice Adjustment' : '声音调节' }}</span>
+                        <a-slider v-model:value="voice_adj" :min="0" :max="10000" :step="1" style="flex: 1;" />
+                        <a-input-number v-model:value="voice_adj" :min="0" :max="10000" :step="1" style="margin-left: 16px" />
+                    </div>
+                    <div style="display: flex; align-items: center; margin-bottom: 10px;">
+                        <span style="margin-right: 10px;">Temperature</span>
+                        <a-slider v-model:value="temperature" :min="0" :max="1" :step="0.1" style="flex: 1;" />
+                        <a-input-number v-model:value="temperature" :min="0" :max="1" :step="0.1" style="margin-left: 16px" />
+                    </div>
+                    <div style="display: flex; align-items: center; margin-bottom: 10px;">
+                        <span style="margin-right: 10px;">Top P</span>
+                        <a-slider v-model:value="top_p" :min="0" :max="1" :step="0.1" style="flex: 1;" />
+                        <a-input-number v-model:value="top_p" :min="0" :max="1" :step="0.1" style="margin-left: 16px" />
+                    </div>
+                    <div style="display: flex; align-items: center; margin-bottom: 10px;">
+                        <span style="margin-right: 10px;">Top K</span>
+                        <a-slider v-model:value="top_k" :min="1" :max="100" :step="1" style="flex: 1;" />
+                        <a-input-number v-model:value="top_k" :min="1" :max="100" :step="1" style="margin-left: 16px" />
+                    </div>
                 </div>
+            </div>
+            <div style="display: flex; justify-content: center; margin-top: 20px;">
+                <a-button type="primary" style="width: 30%;" @click="generate">{{ language === 'en' ? 'Generate' : '生成' }}</a-button>
             </div>
             <br/>
             <br/>
             <div v-if="loading" style="text-align: center;">
                 <a-spin :indicator="indicator" />
+                <p>{{ language === 'en' ? 'Please wait...' : '请稍后...' }}</p>
             </div>
             <div v-if="audioUrl" style="text-align: center;">
-                <p>{{ language === 'en' ? 'Generated Audio:' : '生成的音频:' }}</p>
+                <p>{{ language === 'en' ? 'Generated Audio' : '生成的音频' }}</p>
                 <audio :src="audioUrl" controls></audio>
                 <br/>
                 <a :href="audioUrl" download="generated_audio.wav">
@@ -51,6 +77,11 @@ const voice = ref('man');
 const audioUrl = ref('');
 const loading = ref('');
 const language = ref('en');
+const temperature = ref(0.4)
+const top_p = ref(0.7)
+const top_k = ref(20)
+const voice_adj = ref(2222)
+
 const indicator = h(LoadingOutlined, { style: { fontSize: '50px' } });
 
 const toggleLanguage = () => {
@@ -64,17 +95,21 @@ const addLaugh = () => {
 const addBreak = () => {
     text.value += '[uv_break]';
 };
-
+const setVoiceAdj = (value) => {
+    voice_adj.value = value;
+};
 const generate = async () => {
     loading.value = true;
     audioUrl.value = false;
-    console.log(voice.value);
     try {
         const response = await axios.post(
             'http://127.0.0.1:8000/generate',
             {
                 text: text.value,
-                voice: voice.value
+                voice_adj: voice_adj.value,
+                temperature: temperature.value,
+                top_p: top_p.value,
+                top_k: top_k.value
             },
             {
                 headers: {
@@ -85,8 +120,6 @@ const generate = async () => {
         );
         const url = URL.createObjectURL(new Blob([response.data], { type: 'audio/wav' }));
         audioUrl.value = url;
-        console.log(audioUrl.value);
-        console.log(text.value);
     } catch (error) {
         console.error('生成音频时出错:', error);
     } finally {
